b9aae609e038d7097fc249704eb6d3812e443289
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-22 12:31:41 +0900

    FEAN-57 refactor : 엔티티명 변경 Member -> Auth

--- src/main/java/com/lotte/danuri/auth/AuthRepository.java
+++ src/main/java/com/lotte/danuri/auth/AuthRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthRepository extends JpaRepository<Member, Long> {
+public interface AuthRepository extends JpaRepository<Auth, Long> {
 
-    Optional<Member> findByLoginIdAndDeletedDateIsNull(String id);
+    Optional<Auth> findByLoginIdAndDeletedDateIsNull(String id);
 }
