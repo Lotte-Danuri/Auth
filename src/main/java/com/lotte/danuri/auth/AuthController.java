51f05b9a13dc59e2186c005fcf040ea4f6e482ba
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-06 21:10:43 +0900

    FEAN-57 회원가입 수정 및 JWT 로그인 기능 구현 - refresh 토큰 만료 시 access,refresh token 재발급을 위한 API 구현

--- src/main/java/com/lotte/danuri/auth/AuthController.java
+++ src/main/java/com/lotte/danuri/auth/AuthController.java
@@ -1,43 +1,57 @@
 package com.lotte.danuri.auth;
 
-import com.lotte.danuri.auth.dto.AuthRespDto;
 import com.lotte.danuri.auth.dto.SignUpReqDto;
-import com.lotte.danuri.auth.jwt.JwtService;
-import com.lotte.danuri.auth.jwt.dto.LoginReqDto;
-import com.lotte.danuri.auth.jwt.dto.LoginRespDto;
+import com.lotte.danuri.auth.dto.TokenDto;
+import java.nio.file.AccessDeniedException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/")
 @AllArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-    private final JwtService jwtService;
 
-    @PostMapping("/signup")
+    @PostMapping("/users")
     public ResponseEntity<?> signUp(@RequestBody SignUpReqDto dto) {
-        authService.checkId(dto.getId());
-
-        return new ResponseEntity<>(authService.signUp(dto), HttpStatus.OK);
+        authService.checkId(dto.getId()); // 아이디 중복 체크
+        return new ResponseEntity<>(authService.signUp(dto), HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestHeader(value = "access_token") String accessToken,
+        @RequestHeader(value = "refresh_token") String refreshToken,
+        @RequestBody TokenDto dto)
+        throws AccessDeniedException {
+
+        Long memberId = dto.getMemberId();
+        TokenDto result = authService.refresh(TokenDto.builder()
+            .memberId(memberId)
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build());
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    /*@PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginReqDto dto) {
         AuthRespDto auth = authService.getAuth(dto);
 
         final String token = jwtService.createJwt(auth.getMemberId());
-        final LoginRespDto loginRespDto = LoginRespDto.builder()
+        final LoginRespDto login
+        RespDto = LoginRespDto.builder()
             .name(auth.getName())
             .token(token)
             .build();
 
         return new ResponseEntity<>(loginRespDto, HttpStatus.OK);
-    }
+    }*/
 }
