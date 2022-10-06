2869bb1480c294222bd93a1c1faa69bd183b054d
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-06 21:09:23 +0900

    FEAN-57 JWT 로그인 기능 구현: 비밀번호 암호화를 위한 BCryptPasswordEncoder 빈 등록

--- src/main/java/com/lotte/danuri/auth/AuthApplication.java
+++ src/main/java/com/lotte/danuri/auth/AuthApplication.java
@@ -2,12 +2,22 @@ package com.lotte.danuri.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
