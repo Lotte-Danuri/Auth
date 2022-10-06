65f092faa0a57e67226c8d39ae3e7d8cdfa7108f
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-06 21:26:24 +0900

    FEAN-57 feat: JWT 로그인 구현 - 토큰 발행 담당하는 Provider 구현 및 컴포넌트 등록과 필요 DTO 구현

---
+++ src/main/java/com/lotte/danuri/auth/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.lotte.danuri.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class TokenDto {
+
+    private Long memberId;
+    private String accessToken;
+    private String refreshToken;
+
+}
